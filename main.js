bd24007e34161be3ab645811992712f19fa55e64
Curtis <curtismensah48@gmail.com>
2020-07-25 23:03:06 +0000

    Feature Updates

--- main.js
+++ main.js
@@ -13,8 +13,7 @@ function createWindow() {
     // and load the index.html of the app.
     win.loadFile('index.html')
 
-    // Open the DevTools.
-    win.webContents.openDevTools()
+    win.removeMenu()
 }
 
 // This method will be called when Electron has finished
